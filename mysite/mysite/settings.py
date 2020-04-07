573e6f0ef6d0193ca17e9f4e3773826ca08bc7b5
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-08 02:01:13 +0530

    Implemented Bootstrap with django

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
