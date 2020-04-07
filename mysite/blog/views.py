dd14b71c68dbe678d7df6d43f8035e3723a7a601
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-08 01:26:42 +0530

    Dynamic HTML Data Done

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-# or return render(request, 'index.html', {})
+    posts = Post.objects.all()
+    return render(request, 'index.html', {'posts': posts})                  # to get dynamic html data
+# or return render(request, 'index.html')           # if we do not want dynamic html template
 
 def post(request):
     return HttpResponse("I'm a single post page.")
