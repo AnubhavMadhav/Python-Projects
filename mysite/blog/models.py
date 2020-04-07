2fdbea61bf809a57ca64553d8218ca96943c5d9c
AnubhavMadhav <201851024@iiitvadodara.ac.in>
2020-04-07 18:12:18 +0530

    Initiated my Blog App

---
+++ mysite/blog/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    summary = models.CharField(max_length=300)
+    content = models.TextField()
+    published = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+        def __unicode__(self):
+            return u'%s', self.title
+
+        @property
+        def get_absolute_url(self):
+            return reverse('blog.views.post', args = [self.slug])
